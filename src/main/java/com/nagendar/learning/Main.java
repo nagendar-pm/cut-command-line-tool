ffa635f0e754dc017d755d7907e8cdb00d2ccb86
pagidimarri_nagendar <nagender.pm48@gmail.com>
2023-12-02 11:25:22 +0530

    initial ide commit

---
+++ src/main/java/com/nagendar/learning/Main.java
@@ -0,0 +1,12 @@
+package com.nagendar.learning;
+
+/*
+ * @author: pagidimarri.nagendar
+ * @createdAt: 02/12/23 11:13 am
+ */
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
