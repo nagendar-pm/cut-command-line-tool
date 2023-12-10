58e73519b45a959ba0ccb424c69588e5baaf4f1a
pagidimarri_nagendar <nagender.pm48@gmail.com>
2023-12-10 18:14:53 +0530

    integrated factory for validator and executor components

---
+++ src/main/java/com/nagendar/learning/executor/ExitCommandExecutor.java
@@ -0,0 +1,23 @@
+/*
+ * @author: pagidimarri.nagendar
+ * @createdAt: 10/12/23 6:04 pm
+ */
+
+package com.nagendar.learning.executor;
+
+import com.nagendar.learning.io.Printer;
+import com.nagendar.learning.model.InputCommand;
+
+public class ExitCommandExecutor implements CommandExecutor {
+	private final Printer printer;
+
+	public ExitCommandExecutor(Printer printer) {
+		this.printer = printer;
+	}
+
+	@Override
+	public void execute(InputCommand command) {
+		printer.print("Shutting down...");
+		System.exit(0);
+	}
+}
---
+++ src/main/java/com/nagendar/learning/factory/CommandExecutorFactory.java
@@ -0,0 +1,37 @@
+/*
+ * @author: pagidimarri.nagendar
+ * @createdAt: 10/12/23 6:02 pm
+ */
+
+package com.nagendar.learning.factory;
+
+import com.nagendar.learning.constants.CommonConstants;
+import com.nagendar.learning.executor.CommandExecutor;
+import com.nagendar.learning.executor.CutCommandExecutor;
+import com.nagendar.learning.executor.ExitCommandExecutor;
+import com.nagendar.learning.executor.NonCutCommandExecutor;
+import com.nagendar.learning.io.Printer;
+import com.nagendar.learning.model.InputCommand;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class CommandExecutorFactory {
+	Map<String, CommandExecutor> commandExecutorMap;
+
+	public CommandExecutorFactory(Printer consolePrinter, Printer filePrinter) {
+		this.commandExecutorMap = new HashMap<>();
+		commandExecutorMap.put(CommonConstants.CUT_COMMAND, new CutCommandExecutor(consolePrinter));
+		commandExecutorMap.put(CommonConstants.EXIT_COMMAND, new ExitCommandExecutor(consolePrinter));
+		commandExecutorMap.put(CommonConstants.NON_CUT_COMMAND, new NonCutCommandExecutor(consolePrinter, filePrinter));
+	}
+
+	public CommandExecutor getCommandExecutor(InputCommand command) {
+		CommandExecutor commandExecutor = commandExecutorMap.get(command.getCommandType());
+		if (Objects.isNull(commandExecutor)) {
+			commandExecutor = commandExecutorMap.get(CommonConstants.NON_CUT_COMMAND);
+		}
+		return commandExecutor;
+	}
+}
