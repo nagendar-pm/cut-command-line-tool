e82e17683e254f5e0801744c97cb4ccf75bed71e
pagidimarri_nagendar <nagender.pm48@gmail.com>
2023-12-25 10:46:27 +0530

    integrated the support for `-s` and `-w` flag in `-f` field cut command

--- src/main/java/com/nagendar/learning/option/FieldCutOptionExecutor.java
+++ src/main/java/com/nagendar/learning/option/FieldCutOptionExecutor.java
@@ -1,13 +1,15 @@
 /*
  * @author: pagidimarri.nagendar
- * @createdAt: 16/12/23 12:25 pm
+ * @createdAt: 16/12/23 12:25pm
  */
 
 package com.nagendar.learning.option;
 
+import com.nagendar.learning.exceptions.IllegalFlagException;
 import com.nagendar.learning.factory.PrinterFactory;
 import com.nagendar.learning.io.Printer;
 import com.nagendar.learning.model.Command;
+import com.nagendar.learning.model.Flag;
 import com.nagendar.learning.model.ProcessedCommand;
 import com.nagendar.learning.model.Range;
 
@@ -49,13 +51,36 @@ public class FieldCutOptionExecutor implements OptionExecutor {
 
 	private String handleLine(String line, ProcessedCommand processedCommand) {
 		StringBuilder stringBuilder = new StringBuilder();
+
+		// setting -w flag
+		if (processedCommand.getDelimiter().isEmpty()) {
+			if (processedCommand.getFlags().contains(
+					Flag.FLAG_WHITESPACE_DELIMITER)) {
+				processedCommand.setDelimiter(" ");
+			}
+			else {
+				processedCommand.setDelimiter("\t");
+			}
+		} else if (processedCommand.getFlags().contains(
+				Flag.FLAG_WHITESPACE_DELIMITER)) {
+			throw new IllegalFlagException("Expected either -d or -w, Found both");
+		}
+
 		String[] fields = line.split(processedCommand.getDelimiter());
+
+		// setting -s flag
+		if (processedCommand.getFlags().contains(
+				Flag.FLAG_SUPPRESS_LINES_WITH_NO_FIELD_DELIMITER)) {
+			if (fields.length == 1) {
+				return "";
+			}
+		}
 		for (Range range : processedCommand.getRanges()) {
 			int from = Math.max(1, range.getFrom()) - 1;
 			int to = Math.min(fields.length, range.getTo()) - 1;
 			for (int i=from; i<=to; i++) {
-				if (i > from) stringBuilder.append(processedCommand.getDelimiter());
-				stringBuilder.append(fields[i]);
+				stringBuilder.append(fields[i])
+						.append(processedCommand.getDelimiter());
 			}
 		}
 		return stringBuilder.toString();
